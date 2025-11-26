abb6818c4a019a492ded1a516da541774e1e5f2a
BjarkeZealand <bja001@edu.zealand.dk>
2025-11-26 09:42:39 +0100

    insitution

--- Zealand LoMaS Lib/Repo/InstitutionRepo.cs
+++ Zealand LoMaS Lib/Repo/InstitutionRepo.cs
@@ -1,12 +1,90 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zealand_LoMaS_Lib.Model;
+using Zealand_LoMaS_Lib.Repo.Interfaces;
+
+
 
 namespace Zealand_LoMaS_Lib.Repo
 {
-    public class InstituteRepo
+    public class InstituteRepo : IInstitutionRepo
     {
+        private string _connectionString;
+        public InstituteRepo() 
+        {
+            _connectionString = string.Empty;
+        }
+        private List<Institution> GetInstitutionsByCommand(SqlCommand command)
+        {
+            var institutions = new List<Institution>();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var institution = new Institution
+                    (
+                        (int)reader["InstitutionID"],
+                        //we need a map reader
+                        new Address(),
+                        new List<int>(),
+                        new List<int>()
+                    );
+                    institutions.Add(institution);
+                }
+            }
+            return (institutions);
+        }
+        public void Add(Institution institution)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Institution> GetAll()
+        {
+            var institutions = new List<Institution>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    var command = new SqlCommand("SELECT * FROM Institutions", connection);
+                    connection.Open();
+                    institutions = GetInstitutionsByCommand(command);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Error in GetAll() in InstitutionRepo");
+                    Debug.WriteLine($"Error: {ex}");
+                }
+                finally { connection.Close(); }
+
+
+            }
+            return institutions;
+        }
+
+        public Institution GetByAdminID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Institution GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Institution institution)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Zealand LoMaS Lib/Repo/Interfaces/IInstitutionRepo.cs
+++ Zealand LoMaS Lib/Repo/Interfaces/IInstitutionRepo.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zealand_LoMaS_Lib.Model;
 
 namespace Zealand_LoMaS_Lib.Repo.Interfaces
 {
-    internal interface IInstitutionRepo
+    public interface IInstitutionRepo
     {
+        void Add(Institution institution);
+
+        List<Institution> GetAll();
+
+        Institution GetByID(int id);
+
+        Institution GetByAdminID(int id);
+
+        void Update(Institution institution);
+
+        void DeleteByID(int id);
     }
 }
