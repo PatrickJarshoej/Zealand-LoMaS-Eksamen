69a6f811f81075fb3b2322b6d421b4223771c62f
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-12-05 10:37:11 +0100

    Admin edit UI

--- Zealand LoMaS Lib/Model/Admin.cs
+++ Zealand LoMaS Lib/Model/Admin.cs
@@ -9,7 +9,7 @@ namespace Zealand_LoMaS_Lib.Model
     public class Admin
     {
         public int AdministratorID { get; private set; }
-        public List<int> InstitutionID { get; private set; }
+        public List<int> InstitutionIDs { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public string Email { get; private set; }
@@ -23,20 +23,20 @@ namespace Zealand_LoMaS_Lib.Model
         {
             AdministratorID = administratorID;
         }
-        public Admin(string firstName, string lastName, string email, List<int> institutionID)
+        public Admin(string firstName, string lastName, string email, List<int> institutionIDs)
         {
             FirstName = firstName;
             LastName = lastName;
             Email = email;
-            InstitutionID = institutionID;
+            InstitutionIDs = institutionIDs;
         }
-        public Admin(int adminID,string firstName, string lastName, string email, List<int> institutionID)
+        public Admin(int adminID,string firstName, string lastName, string email, List<int> institutionIDs)
         {
             AdministratorID = adminID;
             FirstName = firstName;
             LastName = lastName;
             Email = email;
-            InstitutionID = institutionID;
+            InstitutionIDs = institutionIDs;
         }
     }
 }
